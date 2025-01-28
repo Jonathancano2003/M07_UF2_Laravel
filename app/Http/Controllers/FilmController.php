25c421f1d26fc6bc566021f0ae5629c8efc6c9c0
Jonathan <canojonathanbenavides@gmail.com>
2025-01-28 15:30:19 +0100

    a

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 use Illuminate\Contracts\View\View;
-=======
->>>>>>> 40e166d2b8a87aeea9fb9d4a489979a5f9a86f81
 use Illuminate\Support\Facades\Storage;
 
 use function PHPUnit\Framework\isNull;
@@ -89,50 +86,6 @@ class FilmController extends Controller
         }
         return view("films.list", ["films" => $films_filtered, "title" => $title]);
     }
-<<<<<<< HEAD
-    public function FilmsByYear($year = null)
-    {
-        $films_filtered = [];
-        $films = FilmController::readFilms();
-        
-        $title = "Listado de todas las películas del año $year";
-
-        foreach ($films as $film) {
-            if ($film['year'] == $year) {
-                $films_filtered[] = $film;
-            }
-        }
-        return view("films.list", ["films" => $films_filtered, "title" => $title]);
-    }
-    public function FilmsByGenre($genre = null)
-    {
-        $films_filtered = [];
-        $films = FilmController::readFilms();
-    
-        // Validar y asignar un valor predeterminado
-        if (is_null($genre)) {
-            $genre = 'Drama'; // Cambia por el género que prefieras como predeterminado
-        }
-    
-        // Definir el título antes del bucle
-        $title = "Listado de todas las películas del género $genre";
-    
-        foreach ($films as $film) {
-            // Comparar género sin importar mayúsculas/minúsculas
-            if (strcasecmp($film['genre'], $genre) == 0) {
-                $films_filtered[] = $film;
-            }
-        }
-    
-        // Mensaje alternativo si no hay películas
-        if (empty($films_filtered)) {
-            $title = "No se encontraron películas para el género $genre";
-        }
-    
-        return view("films.list", ["films" => $films_filtered, "title" => $title]);
-    }
-    
-=======
     public function FilmsByYear(): array
     {
         // Obtener las películas usando readFilms
@@ -165,5 +118,4 @@ class FilmController extends Controller
         // Contar las películas
         return count($films);
     }
->>>>>>> 40e166d2b8a87aeea9fb9d4a489979a5f9a86f81
 }
