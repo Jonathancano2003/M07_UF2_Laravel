8b1e78e8b870cd2d2b37827edc18726aebd31d64
Jonathan <canojonathanbenavides@gmail.com>
2025-02-14 17:04:16 +0100

    44

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Titulo - @yield('titulo')
+    <link rel="stylesheet" href="{{ asset('styles.css') }}">
 
     </title>
 </head>
@@ -12,15 +13,18 @@
 <body>
     @section('header')
     <h1>Header</h1>
+    <img src="{{asset('header.jpg')}}" alt="">
+
     @show
     <hr>
     <div class="container">
         @yield('content')
     </div>
     <hr>
-    div class="container">
+ 
     @section('footer')
     <h1>PIE DE PAGINA </h1>
+   <img src="{{asset('footer.jpg')}}" alt="">
     @show
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,64 +1,55 @@
 @extends('layouts.principal')
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @section('titulo',"Bienvenido a la página de películas")
-    
-    
+@section('titulo', 'Bienvenido a la página de películas')
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+@section('header')
+    @parent
+    <h2>Header de la página de películas</h2>
+    <link rel="stylesheet" href="{{ asset('styles.css') }}">
 
-</head>
+@stop  
 
-<body class="container">
-    <h1 >Añadir peliculas</h1>
+@section('content')
+    <h1>Añadir películas</h1>
     @if (!empty($status))
         <p style="color: red;">{{ $status }}</p>
     @endif
+
+    <h1 class="mt-4">Lista de Películas</h1>
+    <ul>
+        <li><a href="/filmout/oldFilms">Pelis antiguas</a></li>
+        <li><a href="/filmout/newFilms">Pelis nuevas</a></li>
+        <li><a href="/filmout/films">Pelis</a></li>
+        <li><a href="/filmout/films/year/1994">Películas por año</a></li>
+        <li><a href="/filmout/films/genre/drama">Películas por género</a></li>
+        <li><a href="/filmout/films/count">Contador de películas</a></li>
+    </ul>
+
     <form action="{{ route('createFilm') }}" method="POST">
         {{ csrf_field() }}
 
         <label for="nombre">Nombre</label>
-        <input type="text" name="name" />
+        <input type="text" name="name" /><br>
 
         <label for="year">Año</label>
-        <input type="number" name="year" />
+        <input type="number" name="year" /><br>
 
         <label for="genre">Género</label>
-        <input type="text" name="genre" />
+        <input type="text" name="genre" /><br>
 
         <label for="country">País</label>
-        <input type="text" name="country" />
+        <input type="text" name="country" /><br>
 
         <label for="duration">Duración</label>
-        <input type="number" name="duration" />
+        <input type="number" name="duration" /><br>
 
         <label for="img_url">URL de Imagen</label>
-        <input type="text" name="img_url" />
+        <input type="text" name="img_url" /><br>
 
         <button type="submit">Guardar</button>
     </form>
-    <h1 class="mt-4">Lista de Peliculas</h1>
-    <ul>
-        <li><a href=/filmout/oldFilms>Pelis antiguas</a></li>
-        <li><a href=/filmout/newFilms>Pelis nuevas</a></li>
-        <li><a href=/filmout/films>Pelis</a></li>
-        <li><a href="/filmout/films/year/1994">Peliculas por año</a></li>
-        <li><a href="/filmout/films/genre/drama">Peliculas por genre</a></li>
-        <li><a href="/filmout/films/count">Contador de peliculas</a></li>
+@stop
 
-        
-    </ul>
-    <!-- Add Bootstrap JS and Popper.js (required for Bootstrap) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-    <!-- Include any additional HTML or Blade directives here -->
-
-</body>
-
-</html>
+@section('footer')
+    @parent
+@stop
