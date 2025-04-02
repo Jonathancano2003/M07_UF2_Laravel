30d06bbd3a0edc65a4905b7f1986842746eaa85a
Jonathan <canojonathanbenavides@gmail.com>
2025-04-02 18:52:18 +0200

    actor

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Film extends Model
 {
-    use HasFactory;
+    protected $table = 'films';
 
-    protected $fillable = ['name', 'year', 'genre', 'country', 'duration', 'img_url'];
+    protected $fillable = [
+        'name',
+        'year',
+        'genre',
+        'country',
+        'duration',
+        'img_url'
+    ];
+
+    public function actors()
+    {
+        return $this->belongsToMany(Actor::class, 'films_actors');
+    }
 }
